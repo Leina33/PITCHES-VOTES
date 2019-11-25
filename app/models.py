47aea025f24104f4af75dcdcc0e1a2c00bc64e10
Leina33 <dominickrutto@gmail.com>
2019-11-25 15:25:17 +0300

    This code will create a flask migiration file

--- app/models.py
+++ app/models.py
@@ -4,18 +4,21 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
-    roles_id = db.Column(db.Integer,db.Foreignkey('roless.id'))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
     
     def __repr__(self):
         return f'User {self.username}'
 
 #class Role
-class Role(db.model):
-    __table__ = 'roles'
-    
+class Role(db.Model):
+    __tablename__ = 'roles'
+
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
     users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
     def __repr__(self):
         return f'User {self.name}'
     
