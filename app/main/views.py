f0c56f54ab2bcf2966234523d72f114d9b28caf0
Leina33 <dominickrutto@gmail.com>
2019-11-25 12:14:23 +0300

    This are dynamic routes

--- app/main/views.py
+++ app/main/views.py
@@ -10,3 +10,12 @@ def index():
     '''
     message = 'users pitches'
     return render_template('index.html',message = message)
+
+#dynamic route for pitch.html
+@main.route('/pitches/<int:pitches_id>')
+def pitches(pitches_id):
+
+    '''
+    View pitches page function that returns the pitches details page and its data
+    '''
+    return render_template('pitches.html',id = pitches_id)
