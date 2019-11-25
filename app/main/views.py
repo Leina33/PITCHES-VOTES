04215dd4c201b453e9c54b303f82b7f4254f7aa3
Leina33 <dominickrutto@gmail.com>
2019-11-25 16:50:55 +0300

    Import class login_manager

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
 
 # Views
 @main.route('/')
@@ -12,7 +13,8 @@ def index():
     return render_template('index.html',title = title)
 
 #dynamic route for pitch.html
-@main.route('/pitches/<int:pitches_id>')
+@main.route('/pitches/<int:pitches_id>' method = ['GET','POST'])
+@login_required
 def pitches(pitches_id):
 
     '''
