11da7de462031e1c10312d6bbbadc1b1038cca66
Leina33 <dominickrutto@gmail.com>
2019-11-25 14:39:07 +0300

    One to many relationship in database creation between users and roles

--- app/models.py
+++ app/models.py
@@ -4,7 +4,18 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    roles_id = db.Column(db.Integer,db.Foreignkey('roless.id'))
     
     def __repr__(self):
         return f'User {self.username}'
+
+#class Role
+class Role(db.model):
+    __table__ = 'roles'
     
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    
+    return f'User {self.name}'
+ 
